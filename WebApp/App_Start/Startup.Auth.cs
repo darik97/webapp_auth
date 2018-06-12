1ba8c512ec6aaa85d1d5473e7df4f65277727ff1
Daria <dara.darik@gmail.com>
2018-06-12 17:54:31 +0500

    Добавьте файлы проекта.

---
+++ WebApp/App_Start/Startup.Auth.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using Owin.Security.Providers.VKontakte;
+using WebApp.Models;
+
+namespace WebApp
+{
+    public partial class Startup
+    {
+        // Дополнительные сведения о настройке аутентификации см. на странице https://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Настройка контекста базы данных, диспетчера пользователей и диспетчера входа для использования одного экземпляра на запрос
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Включение использования файла cookie, в котором приложение может хранить информацию для пользователя, выполнившего вход,
+            // и использование файла cookie для временного хранения информации о входах пользователя с помощью стороннего поставщика входа
+            // Настройка файла cookie для входа
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Позволяет приложению проверять метку безопасности при входе пользователя.
+                    // Эта функция безопасности используется, когда вы меняете пароль или добавляете внешнее имя входа в свою учетную запись.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });            
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Позволяет приложению временно хранить информацию о пользователе, пока проверяется второй фактор двухфакторной проверки подлинности.
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Позволяет приложению запомнить второй фактор проверки имени входа. Например, это может быть телефон или почта.
+            // Если выбрать этот параметр, то на устройстве, с помощью которого вы входите, будет сохранен второй шаг проверки при входе.
+            // Точно так же действует параметр RememberMe при входе.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            // Раскомментируйте приведенные далее строки, чтобы включить вход с помощью сторонних поставщиков входа
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            app.UseFacebookAuthentication(
+               appId: "212897429322420",
+               appSecret: "71f28de350910785a4af7c5315aec05e");
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "864345624847-lt11mu2q60l8ua0ps6g8g8qu734rt0ir.apps.googleusercontent.com",
+                ClientSecret = "t3vBKSTtqVAHV29i0kW31Q2O"
+            });
+
+            //app.UseVKontakteAuthentication("6603349", "9Epd6n69HpbOV9khdTvW");
+        }
+    }
+}
